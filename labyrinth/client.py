6c04f35dcb21b2ea33f10951a51487ab7199b093
haschtag <juliush532@gmail.com>
2018-05-15 12:11:00 +0100

    main function defined

--- labyrinth/client.py
+++ labyrinth/client.py
@@ -1,17 +1,20 @@
 from socket import socket
 
-host_name, bind_to = 'localhost', 80005
 
-client_socket = socket()
+def main():
+    host_name, bind_to = 'localhost', 80005
+    client_socket = socket()
+    client_socket.connect((host_name, bind_to))
+    to_send = b''
+    while to_send != b'end':
+        to_send = input('> ')
+        to_send = to_send.encode()
+        client_socket.send(to_send)
+        receive = client_socket.recv(1024)
+        print(receive.decode())
+    print('Client closed...')
+    client_socket.close()
 
-client_socket.connect((host_name, bind_to))
-to_send = b''
-while to_send != b'end':
-    to_send = input('> ')
-    to_send = to_send.encode()
-    client_socket.send(to_send)
-    receive = client_socket.recv(1024)
-    print(receive.decode())
 
-print('Client closed...')
-client_socket.close()
+if __name__ == '__main__':
+    main()
